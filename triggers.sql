a8f6cfb9a54f4f1e2e3eebd295203e838feb9745
ssameh12 <shahd.sameh04@gmail.com>
2026-05-27 14:45:16 +0300

    Add files via upload

---
+++ triggers.sql
@@ -0,0 +1,30 @@
+USE ClinicDB;
+
+DELIMITER //
+
+CREATE TRIGGER check_overlapping_appointments
+BEFORE INSERT ON Appointment
+FOR EACH ROW
+BEGIN
+
+    IF EXISTS (
+
+        SELECT 1
+        FROM Appointment
+        WHERE DoctorID = NEW.DoctorID
+          AND AppointmentDate = NEW.AppointmentDate
+          AND NEW.StartTime < EndTime
+          AND NEW.EndTime > StartTime
+
+    )
+
+    THEN
+
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Doctor has overlapping appointment';
+
+    END IF;
+
+END//
+
+DELIMITER ;
